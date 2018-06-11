9e4fd99804dc4f1cf3a13a8bfc5fe75b6ab56130
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-06-12 03:29:02 +0700

    Fix WindowExplorer initial directory

--- src/main/java/ra/sumbayak/ranalyzer/base/WindowExplorer.java
+++ src/main/java/ra/sumbayak/ranalyzer/base/WindowExplorer.java
@@ -2,6 +2,8 @@ package ra.sumbayak.ranalyzer.base;
 
 import java.io.File;
 
+import javax.swing.JFileChooser;
+
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
@@ -12,9 +14,11 @@ public class WindowExplorer {
     
     public WindowExplorer (String title) {
         fileChooser = new FileChooser ();
-        fileChooser.setInitialDirectory (new File ("C:\\Users\\ronald\\Documents\\Kuliah\\Software " +
-                                         "Design\\PPL_D_Kelompok_3(2)"));
         fileChooser.setTitle (title);
+        File ranalyzerProjectsDir = new File (new JFileChooser ().getFileSystemView ().getDefaultDirectory ().toString () + "\\RAnalyzerProjects");
+        if (ranalyzerProjectsDir.exists () || ranalyzerProjectsDir.mkdir ()) {
+            fileChooser.setInitialDirectory (ranalyzerProjectsDir);
+        }
     }
     
     public void addExtensionFilter (String extensionDescription, String... extensions) {
