370a0e397eacc8c7b84034b642f72dc567bef08d
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-06-15 12:08:03 +0700

    Fix viewDependency and checkDependency

--- src/main/java/ra/sumbayak/ranalyzer/controller/DependencyController.java
+++ src/main/java/ra/sumbayak/ranalyzer/controller/DependencyController.java
@@ -1,62 +1,103 @@
 package ra.sumbayak.ranalyzer.controller;
 
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.Viewer.CloseFramePolicy;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ra.sumbayak.ranalyzer.entity.Project;
+import ra.sumbayak.ranalyzer.entity.RequirementDependency;
+import ra.sumbayak.ranalyzer.entity.Statement;
+import ra.sumbayak.ranalyzer.entity.UseCase;
+import ra.sumbayak.ranalyzer.entity.UseCaseDependency;
+import ra.sumbayak.ranalyzer.util.text.TextProcessing;
+import ra.sumbayak.ranalyzer.util.text.wupalmer.WuPalmerCell;
+import ra.sumbayak.ranalyzer.util.text.wupalmer.WuPalmerQueue;
 
 public class DependencyController {
     
     public void viewDependency (@NotNull Project project) {
-        //Graph graph = new SingleGraph ("RequirementDependency Window", false, true);
-        //graph.addAttribute("ui.antialias");
-        //graph.addAttribute("ui.quality");
-        //RequirementDependencyGraph rdGraph = project.getGraph ();
-        //
-        //if (rdGraph == null)
-        //    return;
-        //
-        //for (RequirementDependency dependency : rdGraph.getDependencyList ()) {
-        //    Requirement src = dependency.getSrc ();
-        //    Requirement dst = dependency.getDst ();
-        //
-        //    String id = src.getStatement ().getCode () + "-" + dst.getStatement ().getCode ();
-        //    String type = dependency.getTypeText ();
-        //    String r1 = src.getStatement ().getCode ();
-        //    String r2 = dst.getStatement ().getCode ();
-        //
-        //    Edge edge = graph.addEdge (id, r1, r2);
-        //
-        //    if (edge == null)
-        //        continue;
-        //
-        //    edge.addAttribute ("ui.label", type);
-        //    edge.getNode0 ().addAttribute ("ui.label", src.getStatement ().getCode ().replace ("F", "     R"));
-        //    edge.getNode1 ().addAttribute ("ui.label", dst.getStatement ().getCode ().replace ("F", "     R"));
-        //}
-        //
-        //graph.addAttribute ("ui.stylesheet", "node { fill-color: green; }");
-        //graph.display ().setCloseFramePolicy (CloseFramePolicy.CLOSE_VIEWER);
+        Graph graph = new SingleGraph ("RequirementDependency Window", false, true);
+        graph.addAttribute ("ui.antialias");
+        graph.addAttribute ("ui.quality");
+        
+        List<RequirementDependency> dependencyList = project.getGraph ().getDependencyList ();
+        
+        for (RequirementDependency dependency : dependencyList) {
+            Statement src = dependency.getSrc ();
+            Statement dst = dependency.getDst ();
+
+            String id = "D" + (dependencyList.indexOf (dependency) + 1);
+            String type = dependency.getTypeText ();
+            String r1 = "R" + project.getStatements ().indexOf (src);
+            String r2 = "R" + project.getStatements ().indexOf (dst);
+
+            Edge edge = graph.addEdge (id, r1, r2);
+
+            if (edge == null)
+                continue;
+
+            edge.addAttribute ("ui.label", type);
+            edge.getNode0 ().addAttribute ("ui.label", r1);
+            edge.getNode1 ().addAttribute ("ui.label", r2);
+        }
+
+        graph.addAttribute ("ui.stylesheet", "node { fill-color: green; }");
+        graph.display ().setCloseFramePolicy (CloseFramePolicy.CLOSE_VIEWER);
     }
     
     public void checkDependency (@NotNull Project project) {
-        //RequirementDependencyGraph graph = new RequirementDependencyGraph ();
-        //Random rand = new Random ();
-        //int bound = project.getStatements ().size ();
-        //
-        //for (int i = 0; i < bound; i++) {
-        //    Statement s = project.getStatements ().get (i);
-        //    Statement s1;
-        //
-        //    do s1 = project.getStatements ().get (rand.nextInt (bound));
-        //    while (s == s1);
-        //
-        //    Requirement src = new Requirement (s);
-        //    Requirement dst = new Requirement (s1);
-        //    RequirementDependency d = new RequirementDependency (new Random ().nextInt (5), src, dst);
-        //    graph.addDependency (d);
-        //}
-        //
-        //project.setGraph (graph);
-        //viewDependency (project);
+        List<List<String>> sToken = new ArrayList<> ();
+        List<List<String>> nToken = new ArrayList<> ();
+        List<List<String>> dToken = new ArrayList<> ();
+        
+        for (Statement s : project.getStatements ())
+            sToken.add (TextProcessing.tokenize (s.getValue ()));
+        
+        for (UseCase uc : project.getDiagram ().getUseCaseList ()) {
+            nToken.add (TextProcessing.tokenize (uc.getName ()));
+            dToken.add (TextProcessing.tokenize (uc.getDescription ()));
+        }
+        
+        int row = project.getDiagram ().getUseCaseList ().size ();
+        int col = project.getStatements ().size ();
+        
+        double[][] similarityTable = new double[row][col];
+        double nWeight = 0.5;
+        double dWeight = 0.5;
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                WuPalmerQueue nQueue = WuPalmerQueue.fromToken (nToken.get (i), sToken.get (j));
+                WuPalmerQueue dQueue = WuPalmerQueue.fromToken (dToken.get (i), sToken.get (j));
+                WuPalmerQueue fQueue = WuPalmerQueue.merge (nQueue, dQueue, nWeight, dWeight);
+                similarityTable[i][j] = fQueue.calculateSimilarity ();
+            }
+        }
+        
+        List<WuPalmerCell> filtered = new ArrayList<> ();
+        double threshold = 0.5f;
+        
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (similarityTable[i][j] >= threshold)
+                    filtered.add (new WuPalmerCell (i, j, similarityTable[i][j]));
+        
+        for (WuPalmerCell c1 : filtered) {
+            for (WuPalmerCell c2 : filtered) {
+                if (c1 == c2)
+                    continue;
+                
+                UseCaseDependency ucDependency = project.getDiagram ().getDependency (c1.getRow (), c2.getRow ());
+                if (ucDependency != null)
+                    project.addDependency (ucDependency.getType (), c1.getCol (), c2.getCol ());
+            }
+        }
+        
+        viewDependency (project);
     }
 }
